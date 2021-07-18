482f09ae905c3307eaf3fdf3521e24a2b95fd865
Up & Daun <updauney@daum.net>
2021-07-19 00:27:26 +0900

    python basic review

---
+++ codelion/chinafood_order.py
@@ -0,0 +1,11 @@
+orders = ["짜장", "짬뽕", "탕수육"]
+
+orders.insert(0, "냉면")
+
+orders.append("양장피")
+
+del orders[2]
+
+orders.remove("짜장")
+
+print(orders)
---
+++ codelion/hellomyfriend_2.py
@@ -0,0 +1,9 @@
+# 친구야 안녕 2
+
+food1 = input("내가 먹은 음식의 가격 : ")
+food2 = input("내가 먹은 음식의 가격 : ")
+
+food1 = int(food1)
+food2 = int(food2)
+
+print(food1 + food2)
