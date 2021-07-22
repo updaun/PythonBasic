ae2e6df2c15fc5ea874bd09edccf113c472c2484
Up & Daun <82289435+updaun@users.noreply.github.com>
2021-07-22 18:12:42 +0900

    import json

--- codelion/openAPI_exercise.py
+++ codelion/openAPI_exercise.py
@@ -1,5 +1,16 @@
+import requests
+import json
+
 city = "Seoul"
 apikey = "################################"
 api = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={apikey}"
 
-print(api)
+result = requests.get(api)
+
+print(result.text)
+
+data = json.loads(result.text)
+
+print(type(result.text)) # str
+
+print(type(data)) # dict
