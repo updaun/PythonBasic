51aa538ef874ccadb2eb0992a6a7982090e10375
Up & Daun <82289435+updaun@users.noreply.github.com>
2021-07-25 23:10:00 +0900

    send image

--- codelion/sendemail_smtplib.py
+++ codelion/sendemail_smtplib.py
@@ -29,7 +29,7 @@ SMTP_PORT = 465
 # 메세지 생성
 message = EmailMessage()
 # 메세지 본문
-message.set_content("안녕하세요. 만나서 반갑습니다. 최종 테스트입니다.")
+message.set_content("안녕하세요. 만나서 반갑습니다. https://github.com/updaun 로 놀러오세요!")
 # 메세지 제목
 message["Subject"] = "[Updaun] 제 Github로 초대합니다."
 # 메세지 보내는 사람
@@ -37,18 +37,20 @@ message["From"] = str(GOOGLE_ID)
 # 메세지 받는 사람
 message["To"] = str(OTHER_ID)
 
+# 이미지 파일 읽기
+image = open("codelion/image/walle.jpg", "rb")
 
-
+image.read()
 # 서버에 연결
-smtp = smtplib.SMTP_SSL(SMTP_SERVER, SMTP_PORT)
+# smtp = smtplib.SMTP_SSL(SMTP_SERVER, SMTP_PORT)
 
 # 로그인 테스트
 # print(smtp.login(GOOGLE_ID, GOOGLE_PASSWORD))
 
 # 로그인
-smtp.login(GOOGLE_ID, GOOGLE_PASSWORD)
+# smtp.login(GOOGLE_ID, GOOGLE_PASSWORD)
 
 # 메일 보내기
-smtp.send_message(message)
-print("메일 전송 완료!")
-smtp.quit()
+# smtp.send_message(message)
+# print("메일 전송 완료!")
+# smtp.quit()
